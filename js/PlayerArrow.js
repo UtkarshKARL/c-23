84bbf70d1c404a468b7fc1b064c9912944cc76bd
UtkarshKARL <89794554+UtkarshKARL@users.noreply.github.com>
2022-06-06 14:15:48 +0530

    Add files via upload

---
+++ js/PlayerArrow.js
@@ -0,0 +1,41 @@
+class PlayerArrow {
+  constructor(x, y, width, height, archerAngle) {
+    var options = {
+      isStatic: true,
+      density: 0.1
+    };
+    this.width = width;
+    this.height = height;
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    this.image = loadImage("./assets/arrow.png");
+    this.archerAngle = archerAngle;
+    this.velocity = 0;
+    World.add(world, this.body);
+  }
+
+  shoot(){
+    var newangle = arrow.angle-25;
+    newangle*=(3.14/180);
+    var velocity = p5.Vector.fromAngle(newangle);
+    velocity.mult(0.5);
+
+    Matter.Body.setStatic(this.body,false);
+    Matter.Body.setVelocity(this.body,{x:30,y:-20})
+
+    
+  }
+
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    Push();
+     Translate(pos.x,pos.y);
+     rotate(angle);
+     ImageMode(CENTER);
+     image(this.image,0,0,this.width,this.height);
+    pop();
+   
+
+  }
+}
